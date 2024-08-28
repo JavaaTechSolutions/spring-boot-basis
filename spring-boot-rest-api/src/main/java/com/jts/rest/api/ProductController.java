b0041a11b92b4b2d2d1067dfe2f9e027bfaa81a7
Java Tech Solutions <126051650+JavaaTechSolutions@users.noreply.github.com>
2024-08-28 13:39:27 +0530

    Initial commit

---
+++ spring-boot-rest-api/src/main/java/com/jts/rest/api/ProductController.java
@@ -0,0 +1,39 @@
+package com.jts.rest.api;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+	private final ProductService productService;
+
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@PostMapping("/product")
+	public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+		return ResponseEntity.ok(productService.saveProduct(product));
+	}
+
+	@GetMapping("/products")
+	public List<Product> getAllProducts() {
+		return productService.getAllProducts();
+	}
+
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+		Optional<Product> product = productService.getProductById(id);
+		return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+}
---
+++ spring-boot-rest-api/src/main/java/com/jts/rest/api/ProductService.java
@@ -0,0 +1,28 @@
+package com.jts.rest.api;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+	private final ProductRepository productRepository;
+
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+	
+	public Product saveProduct(Product product) {
+		 return productRepository.save(product);
+	}
+	
+	public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+	
+	public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+}
